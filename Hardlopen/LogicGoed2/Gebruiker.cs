cffb24505aa7d3f66e9ed8df8f652737e37233e7
Ella Zuidema <ella.zuidema@student.fontys.nl>
2018-12-07 14:11:02 +0100

    .Standaard naar .Framework

---
+++ Hardlopen/LogicGoed2/Gebruiker.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Interface_UI_Logic;
+
+namespace Logic
+{
+    public class Gebruiker : IGebruiker
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string Wachtwoord { get; set; }
+        public string Email { get; set; }
+        public double Gewicht { get; set; }
+        public double Lengte { get; set; }
+        public string Geslacht { get; set; }
+
+        public Gebruiker() {}
+
+        public Gebruiker(string naam, string wachtwoord)
+        {
+            Naam = naam;
+            Wachtwoord = wachtwoord;
+        }
+
+        public Gebruiker(int id, string naam, string wachtwoord)
+        {
+            Id = id;
+            Naam = naam;
+            Wachtwoord = wachtwoord;
+        }
+
+        public Gebruiker(string naam, string wachtwoord, string email, string geslacht, double gewicht, double lengte)
+        {
+            Naam = naam;
+            Wachtwoord = wachtwoord;
+            Email = email;
+            Geslacht = geslacht;
+            Gewicht = gewicht;
+            Lengte = lengte;
+        }
+    }
+}
