ae737fdb744a37e796f153c25a78c9e05876b93a
Ella Zuidema <ella.zuidema@student.fontys.nl>
2018-12-11 15:00:20 +0100

    Intergration tests gemaakt

---
+++ Hardlopen/DAL2/ActiviteitDAL.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Model;
+
+namespace DAL
+{
+    public class ActiviteitDal
+    {
+        const string Connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ella\source\repos\Maatwerk-S2\Hardlopen\Hardlopen\App_Data\DatabaseHardlopen.mdf;Integrated Security=True";
+        readonly SqlConnection _conn = new SqlConnection(Connectionstring);
+
+        private void Open()
+        {
+            _conn.Open();
+            Console.WriteLine("Verbonden met " + Connectionstring + ".");
+        }
+
+        public void GegevensInvullen(int tijd, DateTime datum, int afstand, int gebruikerId)
+        {
+            Open();
+            string query = "INSERT INTO dbo.[Loopmoment] (gebruiker, tijd, datum, afstand) VALUES (@Gebruiker, @Tijd, @Datum, @Afstand)";
+            SqlCommand commandInvullen = new SqlCommand(query, _conn);
+            commandInvullen.Parameters.Add("@Gebruiker", SqlDbType.Int).Value = gebruikerId;
+            commandInvullen.Parameters.Add("@Tijd", SqlDbType.Int).Value = tijd;
+            commandInvullen.Parameters.Add("@Datum", SqlDbType.DateTime).Value = datum;
+            commandInvullen.Parameters.Add("@Afstand", SqlDbType.Int).Value = afstand;
+            commandInvullen.ExecuteNonQuery();
+            Close();
+        }
+
+        public virtual List<double> GegevensOverzichtOphalenTijdBar(int id)
+        {
+            List<double> loopmomentOverzichtTijdBar = new List<double>();
+            Open();
+            string query = "SELECT Tijd FROM Loopmoment WHERE Afstand > 1000 AND Gebruiker = @Id";
+            SqlCommand commandOverzicht = new SqlCommand(query, _conn);
+            commandOverzicht.Parameters.Add("@Id", SqlDbType.NChar).Value = id;
+            using (SqlDataReader reader = commandOverzicht.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int tijd = reader.GetInt32(0);
+                    double tijddouble = Convert.ToDouble(tijd);
+                    loopmomentOverzichtTijdBar.Add(tijddouble);
+                }
+            }
+            Close();
+            return loopmomentOverzichtTijdBar;
+        }
+        public virtual List<double> GegevensOverzichtOphalenAfstandBar(int id)
+        {
+            List<double> loopmomentOverzichtAfstandBar = new List<double>();
+            Open();
+            string query = "SELECT Afstand FROM Loopmoment WHERE Afstand > 1000 AND Gebruiker = @Id";
+            SqlCommand commandOverzicht = new SqlCommand(query, _conn);
+            commandOverzicht.Parameters.Add("@Id", SqlDbType.NChar).Value = id;
+            using (SqlDataReader reader = commandOverzicht.ExecuteReader())
+            {
+                loopmomentOverzichtAfstandBar.Clear();
+                while (reader.Read())
+                {
+                    int afstand = reader.GetInt32(0);
+                    double afstanddouble = Convert.ToDouble(afstand);
+                    loopmomentOverzichtAfstandBar.Add(afstanddouble);
+                }
+            }
+            Close();
+            return loopmomentOverzichtAfstandBar;
+        }
+
+        public virtual List<Activiteit> GegevensOverzichtOphalenLine(int id)
+        {
+            List<Activiteit> LoopmomentOverzichtLine = new List<Activiteit>();
+            Open();
+            string query = "SELECT Tijd, Afstand FROM Loopmoment WHERE Afstand > 1000 AND Gebruiker = @Id";
+            SqlCommand commandOverzicht = new SqlCommand(query, _conn);
+            commandOverzicht.Parameters.Add("@Id", SqlDbType.NChar).Value = id;
+            using (SqlDataReader reader = commandOverzicht.ExecuteReader())
+            {
+                LoopmomentOverzichtLine.Clear();
+                while (reader.Read())
+                {
+                    int afstand = reader.GetInt32(0);
+                    int tijd = reader.GetInt32(1);
+                    Activiteit activiteit = new Activiteit(afstand, tijd);
+                    LoopmomentOverzichtLine.Add(activiteit);
+                }
+            }
+            Close();
+            return LoopmomentOverzichtLine;
+        }
+
+        private void Close()
+        {
+            _conn.Close();
+            Console.WriteLine("Verbinding verbroken.");
+        }
+    }
+}
---
+++ Hardlopen/LogicTests/ActiviteitLogicTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using Model;
+using Moq;
+
+namespace Logic.Tests
+{
+    [TestClass()]
+    public class ActiviteitLogicTests
+    {
+        private Mock<ActiviteitDal> _activiteitDalMock;
+        private ActiviteitLogic _activiteitLogic;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _activiteitDalMock = new Mock<ActiviteitDal>();
+            _activiteitLogic = new ActiviteitLogic(_activiteitDalMock.Object);
+        }
+
+        [TestMethod()]
+        public void ToonOverzichtLineTest()
+        {
+            List<Activiteit> overzichtLine = new List<Activiteit>();
+            Activiteit activiteit = new Activiteit(2, 2);
+            Activiteit activiteit2 = new Activiteit(3, 3);
+            overzichtLine.Add(activiteit);
+            overzichtLine.Add(activiteit2);
+            _activiteitDalMock.Setup(m => m.GegevensOverzichtOphalenLine(6)).Returns(overzichtLine);
+            var result = _activiteitLogic.ToonOverzichtLine(6);
+
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod()]
+        public void ToonOverzichtTijdBarTest()
+        {
+            //Setup om andere dal te gebruiken
+            List<double> overzichtTijdBar = new List<double>();
+            overzichtTijdBar.Add(60);
+            overzichtTijdBar.Add(120);
+            _activiteitDalMock.Setup(m => m.GegevensOverzichtOphalenTijdBar(6)).Returns(overzichtTijdBar);
+            var result = _activiteitLogic.ToonOverzichtTijdBar(6);
+
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+
+        [TestMethod()]
+        public void ToonOverzichtAfstandBarTest()
+        {
+            List<double> overzichtAfstandBar = new List<double>();
+            overzichtAfstandBar.Add(1000);
+            overzichtAfstandBar.Add(2000);
+            _activiteitDalMock.Setup(m => m.GegevensOverzichtOphalenAfstandBar(6)).Returns(overzichtAfstandBar);
+            var result = _activiteitLogic.ToonOverzichtAfstandBar(6);
+
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+    }
+}
